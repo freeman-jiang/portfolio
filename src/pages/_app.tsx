c606b5ee124aa6d92ece33bd21ac168358977347
Freeman Jiang <freeman.jiang.ca@gmail.com>
2022-08-15 01:10:19 -0400

    Add basic meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function PortfolioApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Freeman Jiang - Portfolio</title>
+        <meta
+          name="description"
+          content="Freeman Jiang is a CS student at the University of Waterloo and full stack engineer working with TypeScript, Node.js, and React."
+        />
+        <meta charSet="UTF-8" />
+        <meta name="author" content="Freeman Jiang" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default PortfolioApp;
