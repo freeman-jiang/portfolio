878c265f97fbd14b5efd139115fe9bb31e948190
Freeman Jiang <freeman.jiang.ca@gmail.com>
2024-02-15 23:21:04 -0500

    opacity

--- src/components/Gradients.tsx
+++ src/components/Gradients.tsx
@@ -1,6 +1,6 @@
 export const Gradients = () => {
   return (
-    <div className="fixed inset-0 -z-10 opacity-30 md:opacity-50 lg:opacity-60">
+    <div className="fixed inset-0 -z-10 opacity-0 md:opacity-50 lg:opacity-60">
       <div className="absolute top-0 -left-4 h-[70vh] w-[18vw] animate-blob rounded-full bg-emerald-200 mix-blend-multiply blur-3xl filter"></div>
       <div className="animation-delay-2000 absolute top-0 -right-4 h-[60vh] w-[18vw] animate-blob rounded-full bg-indigo-200 mix-blend-multiply blur-3xl filter"></div>
       <div className="animation-delay-4000 absolute -bottom-8 left-20 h-[60vh] w-[18vw] animate-blob rounded-full bg-pink-200 mix-blend-multiply blur-3xl filter"></div>
