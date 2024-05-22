84ea31145e4db712280b4ebe45293ae58e619203
Freeman Jiang <freeman.jiang.ca@gmail.com>
2024-05-22 01:56:16 -0700

    always vertical stack

--- src/components/Positions.tsx
+++ src/components/Positions.tsx
@@ -11,63 +11,47 @@ interface Props {
 }
 
 export const Positions = ({ positions }: Props) => {
-  const renderRow = ([p1, p2]: IPosition[]) => {
-    const p1Fields = p1.fields as IPositionFields;
-    const p2Fields = p2.fields as IPositionFields;
+  const renderRow = (positions: IPosition[]) => {
+    const p1Fields = positions[0].fields as IPositionFields;
+    const p2Fields = positions[1]?.fields as IPositionFields | undefined;
     return (
       <div
         className="flex flex-col gap-4 md:flex-row md:gap-0"
-        key={`${p1Fields.companyUrl}-${p2Fields.companyUrl}`}
+        key={
+          p2Fields
+            ? `${p1Fields.companyUrl}-${p2Fields.companyUrl}`
+            : p1Fields.companyUrl
+        }
       >
         <section className="md:w-1/2">
           <Position key={p1Fields.companyUrl} {...p1Fields} />
         </section>
-        <div className="hidden px-3.5 md:block"></div>
-        <section className="md:w-1/2">
-          <Position key={p2Fields.companyUrl} {...p2Fields} />
-        </section>
+        {p2Fields && (
+          <>
+            <div className="hidden px-3.5 md:block"></div>
+            <section className="md:w-1/2">
+              <Position key={p2Fields.companyUrl} {...p2Fields} />
+            </section>
+          </>
+        )}
       </div>
     );
   };
 
   const { pastPositions, currentPositions } = positions;
 
-  if (!currentPositions) {
-    // Chunk past positions into groups of 2 for 2 column layout
-    const pairs = chunkArray(pastPositions, 2);
-
-    return (
-      <div className="mt-12">
-        <h2 className="text-3xl font-semibold tracking-tight">Past</h2>
-        <div className="mt-4 flex flex-col gap-4">{pairs.map(renderRow)}</div>
-      </div>
-    );
-  }
+  // Chunk past positions into groups of 2 for 2 column layout
+  const currentPairs = currentPositions ? chunkArray(currentPositions, 2) : [];
+  const pastPairs = chunkArray(pastPositions, 2);
 
   return (
-    <>
-      <section className="mt-12 md:w-1/2">
-        <h2 className="text-3xl font-semibold tracking-tight">Currently</h2>
-        <div className="mt-4 flex flex-col gap-4">
-          {currentPositions.map((position) => (
-            <Position
-              key={position.sys.id}
-              {...(position.fields as IPositionFields)}
-            />
-          ))}
-        </div>
-      </section>
-      <section className="mt-12 md:w-1/2">
-        <h2 className="text-3xl font-semibold tracking-tight">Past</h2>
-        <div className="mt-4 flex flex-col gap-4">
-          {pastPositions?.map((position) => (
-            <Position
-              key={position.sys.id}
-              {...(position.fields as IPositionFields)}
-            />
-          ))}
-        </div>
-      </section>
-    </>
+    <div className="mt-10">
+      <h2 className="text-3xl font-semibold tracking-tight">Now</h2>
+      <div className="mt-4 flex flex-col gap-4">
+        {currentPairs.map(renderRow)}
+      </div>
+      <h2 className="text-3xl font-semibold tracking-tight mt-8">Past</h2>
+      <div className="mt-4 flex flex-col gap-4">{pastPairs.map(renderRow)}</div>
+    </div>
   );
 };
